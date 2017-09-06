e7c04f5b516546830216eb7b6fe3d2ae2628de8a
Vu Nguyen Tran <camping89@gmail.com>
2017-09-06 13:27:29 +0700

    null check

--- Reebonz.Marketplace.Clients.Net/ServiceConsumers/BaseApiConsumer.cs
+++ Reebonz.Marketplace.Clients.Net/ServiceConsumers/BaseApiConsumer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using Newtonsoft.Json;
 using Reebonz.Marketplace.Clients.Net.Entities;
+using Reebonz.Marketplace.Clients.Net.Extensions;
 using RestSharp;
 
 namespace Reebonz.Marketplace.Clients.Net.ServiceConsumers
@@ -23,47 +24,55 @@ namespace Reebonz.Marketplace.Clients.Net.ServiceConsumers
                 StatusCode = response.StatusCode
             };
 
+            var content = response.Content;
             switch (response.StatusCode)
             {
                 case HttpStatusCode.OK:
-                    if (usesApiResponse)
+                    if (content.IsNotNullOrEmpty())
                     {
-                        var apiResponse = JsonConvert.DeserializeObject<ApiResponse<T>>(response.Content);
-                        res.Resource = apiResponse.Resource;
-                        res.ResourceId = apiResponse.ResourceId;
-                        res.Warnings = apiResponse.Warnings;
-                    }
-                    else
-                    {
-                        res.Resource = JsonConvert.DeserializeObject<T>(response.Content);
+                        if (usesApiResponse)
+                        {
+                            var apiResponse = JsonConvert.DeserializeObject<ApiResponse<T>>(content);
+                            res.Resource = apiResponse.Resource;
+                            res.ResourceId = apiResponse.ResourceId;
+                            res.Warnings = apiResponse.Warnings;
+                        }
+                        else
+                        {
+                            res.Resource = JsonConvert.DeserializeObject<T>(content);
+                        }
                     }
                     break;
                 case HttpStatusCode.Created:
-                    if(usesApiResponse)
+                    if (content.IsNotNullOrEmpty())
                     {
-                        var apiResponse = JsonConvert.DeserializeObject<ApiResponse<T>>(response.Content);
-                        res.Resource = apiResponse.Resource;
-                        res.ResourceId = apiResponse.ResourceId;
-                        res.Warnings = apiResponse.Warnings;
-                    }
-                    else
-                    {
-                        res.Resource = JsonConvert.DeserializeObject<T>(response.Content);
+                        if (usesApiResponse)
+                        {
+                            var apiResponse = JsonConvert.DeserializeObject<ApiResponse<T>>(content);
+                            res.Resource = apiResponse.Resource;
+                            res.ResourceId = apiResponse.ResourceId;
+                            res.Warnings = apiResponse.Warnings;
+                        }
+                        else
+                        {
+                            res.Resource = JsonConvert.DeserializeObject<T>(content);
+                        }
                     }
                     break;
                 case HttpStatusCode.NoContent:
                     break;
                 case HttpStatusCode.NotFound:
                 case HttpStatusCode.BadRequest:
+                    if (content.IsNotNullOrEmpty())
                     {
-                        var er = JsonConvert.DeserializeObject<ApiErrorResponse>(response.Content);
+                        var er = JsonConvert.DeserializeObject<ApiErrorResponse>(content);
                         res.Errors = er.Errors;
                         res.Warnings = er.Warnings;
                     }
                     break;
             }
 
-            if(ThrowOnErrorStatus)
+            if (ThrowOnErrorStatus)
                 throw new ApiException(res, "Error response from Marketplace API, see Response property for details");
 
             return res;
